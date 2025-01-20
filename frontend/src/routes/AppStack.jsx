6aa4575885322c347d648354abc90792171e2c62
abhy <b23cs1085@iitj.ac.in>
2025-01-20 17:15:00 +0530

    feat: add vector icons dependency, enhance Dashboard layout with buttons and custom header

--- frontend/src/routes/AppStack.jsx
+++ frontend/src/routes/AppStack.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -15,10 +15,37 @@ import Profile from '../screens/Profile';
 // Create Bottom Tab Navigator
 const Tab = createBottomTabNavigator();
 
+const getDate = () => {
+  const date = new Date();
+  const options = { month: 'short', day: '2-digit', year: 'numeric' };
+  const formattedDate = date.toLocaleDateString('en-US', options);
+  return formattedDate.replace(/\s(\d{4})$/, ' $1'); 
+};
+
+const CustomHeader = () => {
+  return (
+    <View style={styles.headerContainer}>
+      <Text style={styles.dateText}>{getDate()}</Text>
+      <Text style={styles.titleText}>Dashboard</Text>
+    </View>
+  );
+};
+
+
+
 const MyTabs = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Dashboard} />
+      <Tab.Screen name="Home" component={Dashboard}
+        options={{
+          headerTitle: () => <CustomHeader />,
+          headerStyle: {
+            borderBottomWidth: 0,  // Remove the bottom border
+            elevation: 0,  // Remove shadow on Android
+            shadowOpacity: 0,  // Remove shadow on iOS
+            height:200
+          },
+        }} />
       <Tab.Screen name="LabourDashboard" component={LabourDashboard} />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
@@ -47,4 +74,22 @@ const AppStack = () => {
 
 export default AppStack;
 
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    marginTop:20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    marginLeft:20
+  },
+  dateText: {
+    fontSize: 16,
+    color: 'gray',
+    marginBottom:20
+  },
+  titleText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom:20
+  },
+});
