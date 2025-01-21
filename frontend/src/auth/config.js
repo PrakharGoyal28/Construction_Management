6b98e48de0dd8a4ade54dcda0bf75386ee53ea3a
abhy <b23cs1085@iitj.ac.in>
2025-01-21 22:29:21 +0530

    modify the labour contoler to also send absent number

--- frontend/src/auth/config.js
+++ frontend/src/auth/config.js
@@ -1,3 +1,3 @@
-const AbhyIp="10.23.33.39:3000"
+const AbhyIp="172.31.89.54:3000"
 const PrakharIp="172.31.89.30:8081"
-export const BASE_URL=`http://${PrakharIp}/api/v1`
+export const BASE_URL=`http://${AbhyIp}/api/v1`
