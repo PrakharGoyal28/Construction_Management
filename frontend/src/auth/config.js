91f7ba24a36617cc583e5c87b35cdf5a799cfdfb
PrakharGoyal28 <PRAKHAR28GOYAL@GMAIL.COM>
2025-01-28 15:55:34 +0530

    changes

--- frontend/src/auth/config.js
+++ frontend/src/auth/config.js
@@ -1,8 +1,4 @@
 const AbhyIp="172.31.89.54:3000"
 const PrakharIp="172.31.89.30:8081"
 const MayankIp="172.31.89.215:8081"
-<<<<<<< Updated upstream
-export const BASE_URL=`https://construction-management-zofi.onrender.com/api/v1`
-=======
 export const BASE_URL="https://construction-management-zofi.onrender.com/api/v1"
->>>>>>> Stashed changes
--- server/src/routes/labour.routes.js
+++ server/src/routes/labour.routes.js
@@ -13,11 +13,7 @@ import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-<<<<<<< Updated upstream
-// router.post("/register",upload.single('image'), registerLabour);
-=======
 router.post("/register",upload.single("image"), registerLabour);
->>>>>>> Stashed changes
 // Route to update attendance for a specific labour
 router.post("/updateAttendance", verifyJWT, checkRole, updateAttendance);
 // Route to get attendance history for a specific labour
