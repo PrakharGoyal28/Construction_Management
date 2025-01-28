4d9a25eb4e20747a6c805d4843bfde2375a80e19
abhy <b23cs1085@iitj.ac.in>
2025-01-28 12:09:24 +0530

    error fixed

--- server/src/controllers/report.controller.js
+++ server/src/controllers/report.controller.js
@@ -1,5 +1,5 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-import { Report } from "../models/report.model.js";
+import { Report } from "../models/Report.model.js";
 import {ApiError} from "../utils/ApiError.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 
--- server/src/controllers/transaction.controller.js
+++ server/src/controllers/transaction.controller.js
@@ -1,5 +1,5 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-import { Transaction } from "../models/transaction.model.js";
+import { Transaction } from "../models/Transaction.model.js";
 import {ApiError} from "../utils/ApiError.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 
