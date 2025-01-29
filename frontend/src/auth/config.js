c6ac1b0a794582ffc45178e133d7a19e39e7d686
abhy <b23cs1085@iitj.ac.in>
2025-01-29 07:13:06 +0530

    error fifex

--- frontend/src/auth/config.js
+++ frontend/src/auth/config.js
@@ -1,4 +1,4 @@
 const AbhyIp="172.31.89.54:3001"
 const PrakharIp="172.31.89.30:3000"
 const MayankIp="172.31.89.215:3000"
-export const BASE_URL=`http://${MayankIp}/api/v1`
+export const BASE_URL=`http://${AbhyIp}/api/v1`
